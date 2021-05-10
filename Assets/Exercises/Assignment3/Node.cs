0a70fcc994c9ffd4cfb0607de3fa5e6eb3d49851
sparkolo <albertogiudice@hotmail.it>
2021-05-10 03:24:35 +0200

    Solved assignment 3

--- Assets/Exercises/Assignment3/Node.cs
+++ Assets/Exercises/Assignment3/Node.cs
@@ -35,6 +35,11 @@ namespace AfGD.Assignment3
         // Used for debug purposes.
         Color m_Color;
 
+        private float minSplitPerc = 0.25f;
+        private float minRoomPerc = .5f;
+        private float minRoomBorder = 1.5f;
+        private float hallwaySize = 0.5f;
+
         public bool IsConnected { get; private set; }
         public bool IsLeafNode => m_ChildA == null || m_ChildB == null;
 
@@ -92,13 +97,33 @@ namespace AfGD.Assignment3
         {
 #if SOLUTION_3_1
             // Randomly choose an axis to split on
+            Axis axis = (Random.value < 0.5f) ? Axis.X : Axis.Z;
 
             // Partition the Cell into two cells; cellA & cellB.
+            Vector3 cellMin = m_Cell.min;
+            Vector3 cellMax = m_Cell.max;
+            Vector3 splitPoint;
+            float cellSizeAxis = m_Cell.size[(int)axis];
+            float newCellMinDelta = minSplitPerc * cellSizeAxis;
+            float splitPos = Random.Range(cellMin[(int)axis] + newCellMinDelta, cellMax[(int)axis] - newCellMinDelta);
+
+            splitPoint = cellMax;
+            splitPoint[(int)axis] = splitPos;
+            Bounds boundA = new Bounds { min = cellMin, max = splitPoint };
+            Node cellA = new Node(boundA);
+
+            splitPoint = cellMin;
+            splitPoint[(int)axis] = splitPos;
+            Bounds boundB = new Bounds { min = splitPoint, max = cellMax };
+            Node cellB = new Node(boundB);
 
             // Only if we can split into two valid cells do we actually proceed with the split
-            // m_ChildA = ...;
-            // m_ChildB = ...;
-
+            if(cellA.IsValidCell() && cellB.IsValidCell())
+            {
+                m_SplitAxis = axis;
+                m_ChildA = cellA;
+                m_ChildB = cellB;
+            }
 #endif
 
         }
@@ -124,9 +149,19 @@ namespace AfGD.Assignment3
         {
 #if SOLUTION_3_2
             // Randomly generate bounds for the room
+            Vector3 cellSize = m_Cell.size;
+            float sizeX = Random.Range(minRoomPerc * cellSize.x, cellSize.x - 2 * minRoomBorder);
+            float sizeZ = Random.Range(minRoomPerc * cellSize.z, cellSize.z - 2 * minRoomBorder);
 
-            // m_Room = ... (todo)
+            Vector3 roomSize = new Vector3(sizeX, cellSize.y, sizeZ);
 
+            float cornerX = Random.Range(minRoomBorder, cellSize.x - roomSize.x - minRoomBorder);
+            float cornerZ = Random.Range(minRoomBorder, cellSize.z - roomSize.z - minRoomBorder);
+
+            Vector3 roomMin = m_Cell.min + new Vector3(cornerX, 0f, cornerZ);
+            Vector3 roomMax = m_Cell.min + new Vector3(cornerX + roomSize.x, roomSize.y, cornerZ + roomSize.z);
+
+            m_Room = new Bounds { min = roomMin, max = roomMax };
 #endif
 
             // Spawn Mesh that represents room
@@ -191,7 +226,44 @@ namespace AfGD.Assignment3
             //        (or another hallway if the connecting child node has child nodes itself))
             // 6) Create a Cube that represents the hallway (i.e. GameObject.CreatePrimitive(PrimitiveType.Cube);)
 #if SOLUTION_3_3
+            Bounds roomA = m_ChildA.Room;
+            Bounds roomB = m_ChildB.Room;
+            Axis overlapAxis = m_SplitAxis == Axis.X ? Axis.Z : Axis.X;
 
+            Vector2 rangeA = new Vector2(roomA.min[(int)overlapAxis] + hallwaySize, roomA.max[(int)overlapAxis] - hallwaySize);
+            Vector2 rangeB = new Vector2(roomA.min[(int)overlapAxis] + hallwaySize, roomB.max[(int)overlapAxis] - hallwaySize);
+            Vector2 overlapRange = new Vector2(Mathf.Max(rangeA.x, rangeB.x), Mathf.Min(rangeA.y, rangeB.y));
+
+            // Overlap successful
+            if(overlapRange.x < overlapRange.y)
+            {
+                float overlapPos = Random.Range(overlapRange.x, overlapRange.y);
+                Vector3 connectionStart = roomA.max;
+                Vector3 connectionEnd = roomB.min;
+                connectionStart[(int)overlapAxis] = overlapPos - hallwaySize;
+                connectionEnd[(int)overlapAxis] = overlapPos + hallwaySize;
+                connectionStart.y = roomA.center.y - hallwaySize;
+                connectionEnd.y = roomA.center.y + hallwaySize;
+
+                Vector3 rayOrigin = (connectionEnd + connectionStart) / 2.0f;
+                Vector3 connectionDirection = m_SplitAxis == Axis.X ? Vector3.right : Vector3.forward;
+
+                Physics.Raycast(rayOrigin, connectionDirection, out var leftHall);
+                Physics.Raycast(rayOrigin, -connectionDirection, out var rightHall);
+
+                connectionStart[(int)m_SplitAxis] = rightHall.point[(int)m_SplitAxis];
+                connectionEnd[(int)m_SplitAxis] = leftHall.point[(int)m_SplitAxis];
+
+                Bounds hallwayBound = new Bounds { min = connectionStart, max = connectionEnd };
+
+                // Spawn Mesh that represents hallway
+                GameObject hallway = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                hallway.transform.position = hallwayBound.center;
+                hallway.transform.localScale = hallwayBound.size;
+                hallway.GetComponent<MeshRenderer>().material.color = Color.gray;
+                hallway.name = "Hallway";
+            }
+            // TODO: Handle the case of no straight line overlap
 #endif
         }
 
